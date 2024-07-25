71e667c3ad4f38c9af08189a3aa853ba142457de
iskkang <76502863+iskkang@users.noreply.github.com>
2024-07-25 13:51:53 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 let corsOptions = {
     origin: '*',
