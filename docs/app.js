bbc0e6ea0b425d799a38073f0137bcd3fed9674d
iskkang <76502863+iskkang@users.noreply.github.com>
2024-07-26 00:34:50 +0900

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -153,10 +153,18 @@ function initializeMap(portMapData) {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
+    // Define custom icon
+    var portIcon = L.icon({
+        iconUrl: 'docs/port-top.png',
+        iconSize: [30, 30], // size of the icon
+        iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
+        popupAnchor: [0, -15] // point from which the popup should open relative to the iconAnchor
+    });
+
     // Add port icons
     if (portMapData && portMapData.length > 0) {
         portMapData.forEach(item => {
-            const marker = L.marker(item.coord.split(',').map(Number)).addTo(map);
+            const marker = L.marker(item.coord.split(',').map(Number), { icon: portIcon }).addTo(map);
             marker.bindPopup(`
                 <h4>${item.name}</h4>
                 <p>Country: ${item.country}</p>
@@ -165,6 +173,7 @@ function initializeMap(portMapData) {
         });
     }
 }
+
 
 async function fetchPortDetails(locode) {
     try {
