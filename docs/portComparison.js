b547ca1e53d70a74791865450943aeca2d096206
iskkang <76502863+iskkang@users.noreply.github.com>
2024-08-05 11:35:46 +0900

    Update portComparison.js

--- docs/portComparison.js
+++ docs/portComparison.js
@@ -1,39 +1,35 @@
 async function renderPortComparisonChart() {
-    const url = "https://www.econdb.com/widgets/top-port-comparison/data/";
-    const portComparisonData = await fetchData(url);
-    
+    const portComparisonData = await fetchData('port-comparison');
     if (portComparisonData && portComparisonData.length > 0) {
         const portNames = portComparisonData.map(item => item.name);
-        const portValuesJune24 = portComparisonData.map(item => parseFloat(item['June 24']));
-        const portValuesJune23 = portComparisonData.map(item => parseFloat(item['June 23']));
+        const portValuesJune24 = portComparisonData.map(item => item['June 24']);
+        const portValuesJune23 = portComparisonData.map(item => item['June 23']);
 
         const portTraceJune24 = {
-            y: portNames,
-            x: portValuesJune24,
+            y: portNames,  // x축을 y축으로 변경
+            x: portValuesJune24,  // y축을 x축으로 변경
             type: 'bar',
-            orientation: 'h',
             name: 'June 24',
-            marker: { color: 'orange' }
+            marker: { color: 'orange' },
+            orientation: 'h'  // 수평 막대 그래프
         };
 
         const portTraceJune23 = {
-            y: portNames,
-            x: portValuesJune23,
+            y: portNames,  // x축을 y축으로 변경
+            x: portValuesJune23,  // y축을 x축으로 변경
             type: 'bar',
-            orientation: 'h',
             name: 'June 23',
-            marker: { color: 'blue' }
+            marker: { color: 'blue' },
+            orientation: 'h'  // 수평 막대 그래프
         };
 
         const portLayout = {
             title: '',
             xaxis: { title: 'Thousand TEU' },
-            yaxis: { title: 'Port' },
-            barmode: ''
+            yaxis: { title: '' },
+            barmode: 'group'
         };
 
         Plotly.newPlot('portComparisonChart', [portTraceJune24, portTraceJune23], portLayout);
     }
 }
-
-renderPortComparisonChart();
