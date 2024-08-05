177474951b92e2deba879f7e40411993636b0188
iskkang <76502863+iskkang@users.noreply.github.com>
2024-08-05 11:53:06 +0900

    Update portComparison.js

--- docs/portComparison.js
+++ docs/portComparison.js
@@ -2,23 +2,23 @@ async function renderPortComparisonChart() {
     const portComparisonData = await fetchData('port-comparison');
     if (portComparisonData && portComparisonData.length > 0) {
         const portNames = portComparisonData.map(item => item.name);
-        const portValuesJune24 = portComparisonData.map(item => item['June 24']);
-        const portValuesJune23 = portComparisonData.map(item => item['June 23']);
+        const portValues24 = portComparisonData.map(item => item['June 24']);
+        const portValues23 = portComparisonData.map(item => item['June 23']);
 
-        const portTraceJune24 = {
+        const portTrace24 = {
             y: portNames,  // x축을 y축으로 변경
             x: portValuesJune24,  // y축을 x축으로 변경
             type: 'bar',
-            name: 'June 24',
+            name: '24',
             marker: { color: 'orange' },
             orientation: 'h'  // 수평 막대 그래프
         };
 
-        const portTraceJune23 = {
+        const portTrace23 = {
             y: portNames,  // x축을 y축으로 변경
             x: portValuesJune23,  // y축을 x축으로 변경
             type: 'bar',
-            name: 'June 23',
+            name: '23',
             marker: { color: 'blue' },
             orientation: 'h'  // 수평 막대 그래프
         };
@@ -30,6 +30,6 @@ async function renderPortComparisonChart() {
             barmode: 'group'
         };
 
-        Plotly.newPlot('portComparisonChart', [portTraceJune24, portTraceJune23], portLayout);
+        Plotly.newPlot('portComparisonChart', [portTrace24, portTrace23], portLayout);
     }
 }
