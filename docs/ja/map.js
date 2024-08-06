3f8e65b4026d44f7623b59a8dff468f1171edff5
iskkang <76502863+iskkang@users.noreply.github.com>
2024-08-06 15:13:52 +0900

    Update map.js

--- docs/ja/map.js
+++ docs/ja/map.js
@@ -1,4 +1,8 @@
-function initializeMap() {
+document.addEventListener('DOMContentLoaded', () => {
+    initializeMap();
+});
+
+async function initializeMap() {
     var map = L.map('map').setView([17.35344883620718, 5.734691], 2);
     L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png', {
         maxZoom: 12,
@@ -6,25 +10,24 @@ function initializeMap() {
     }).addTo(map);
 
     // Fetch port map data and add markers
-    fetchPortMapData().then(portMapData => {
-        if (portMapData && portMapData.length > 0) {
-            var portIcon = L.icon({
-                iconUrl: 'port-top.png',
-                iconSize: [20, 20], // size of the icon
-                iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
-                popupAnchor: [0, -15] // point from which the popup should open relative to the iconAnchor
-            });
+    const portMapData = await fetchPortMapData();
+    if (portMapData && portMapData.length > 0) {
+        var portIcon = L.icon({
+            iconUrl: 'port-top.png',
+            iconSize: [20, 20], // size of the icon
+            iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
+            popupAnchor: [0, -15] // point from which the popup should open relative to the iconAnchor
+        });
 
-            portMapData.forEach(item => {
-                const marker = L.marker(item.coord.split(',').map(Number), { icon: portIcon }).addTo(map);
-                marker.bindPopup(`
-                    <h4>${item.name}</h4>
-                    <p>Country: ${item.country}</p>
-                    <p>Rank: ${item.rank}</p>
-                `);
-            });
-        }
-    });
+        portMapData.slice(0, 10).forEach(item => { // Show only the most recent 10 items
+            const marker = L.marker(item.coord.split(',').map(Number), { icon: portIcon }).addTo(map);
+            marker.bindPopup(`
+                <h4>${item.name}</h4>
+                <p>Country: ${item.country}</p>
+                <p>Rank: ${item.rank}</p>
+            `);
+        });
+    }
 }
 
 async function fetchPortMapData() {
@@ -37,7 +40,7 @@ async function fetchPortMapData() {
             console.error('Failed to fetch port map data:', response.status);
         }
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Error fetching port map data:', error);
     }
     return null;
 }
